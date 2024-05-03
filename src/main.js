f967fe3da15b82ac94778620f9a9b10bb7a969da
kkocdko <kkocdko@163.com>
2024-05-04 02:00:01 +0800

    the exclude import proposal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { app, protocol, BrowserWindow } from "electron";
+// @ts-check
+// import { app, protocol, BrowserWindow } from "electron";
 import { fileURLToPath } from "node:url";
 import { readFile } from "node:fs/promises";
+import fs from "node:fs";
+import path from "node:path";
+import { createServer } from "node:http";
+import { spawn } from "node:child_process";
+
+// 会排除源码中导入路径满足 regexp 的静态 import 语句，然后返回排除后的源码。被排除的将成为 `// excluded: import xxx form ...`
+const excludeImport = (sourceCode, regexp) => {
+  // 先跳过所有注释
+  // 然后找 import
+  // 直到找不到？
+};
 
 if (globalThis.document) {
   // is in renderer
@@ -8,85 +20,86 @@ if (globalThis.document) {
 } else {
   // is in main
 
-  const html = ([s]) => s;
+  const html = (/** @type {any} */ [s]) => s;
   const page = () => html`
     <!DOCTYPE html>
     <head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width" />
       <meta name="color-scheme" content="light dark" />
+      <script type="module" src="/main.js"></script>
       <title>clevert</title>
     </head>
-    <body>
-      <div id="root"></div>
-      <script type="module" src="/main.js"></script>
-    </body>
+    <body></body>
   `;
 
-  const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 1280,
-      height: 720,
-      title: "clevert",
-      webPreferences: {
-        // nodeIntegration: true,
-        contextIsolation: false,
-        webSecurity: false,
-        sandbox: false,
-        // preload: fileURLToPath(import.meta.url),
-      },
-      autoHideMenuBar: true,
-    });
-    win.loadURL("resource:///main.html");
-    win.webContents.openDevTools();
-  };
-
-  app.whenReady().then(() => {
-    protocol.handle("resource", async (req) => {
-      if (req.url === "resource:///main.html") {
-        const type = "text/html; charset=utf-8";
-        return new Response(new Blob([page()], { type }));
-      }
-      if (req.url === "resource:///main.js") {
-        const buffer = await readFile(fileURLToPath(import.meta.url));
-        const type = "text/javascript; charset=utf-8";
-        return new Response(new Blob([buffer], { type }));
-      }
-      return new Response(new Blob(["not found"], { type: "text/plain" }));
-    });
-    createWindow();
-    // mac
-    app.on("activate", () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-      }
-    });
-  });
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-      app.quit();
+  const server = createServer(async (req, res) => {
+    console.log({ url: req.url });
+    if (req.url === "/") {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.writeHead(200);
+      res.end(page());
+      return;
     }
+    if (req.url === "/main.js") {
+      res.setHeader("Content-Type", "text/javascript; charset=utf-8");
+      res.writeHead(200);
+      const buffer = await readFile(fileURLToPath(import.meta.url));
+      res.end(buffer.toString());
+      return;
+    }
+    res.writeHead(404).end("not found");
   });
+
+  server.listen(9393, "127.0.0.1");
+
+  // const createWindow = () => {
+  //   const win = new BrowserWindow({
+  //     width: 1280,
+  //     height: 720,
+  //     title: "clevert",
+  //     webPreferences: {
+  //       // nodeIntegration: true,
+  //       contextIsolation: false,
+  //       webSecurity: false,
+  //       sandbox: false,
+  //       // preload: fileURLToPath(import.meta.url),
+  //     },
+  //     autoHideMenuBar: true,
+  //   });
+  //   win.loadURL("resource:///main.html");
+  //   win.webContents.openDevTools();
+  // };
+
+  // app.whenReady().then(() => {
+  //   protocol.handle("resource", async (req) => {
+  //     console.log(req.url)
+  //     if (req.url === "resource:///main.html") {
+  //       const type = "text/html; charset=utf-8";
+  //       return new Response(new Blob([page()], { type }));
+  //     }
+  //     if (req.url === "resource:///main.js") {
+  //       const buffer = await readFile(fileURLToPath(import.meta.url));
+  //       const type = "text/javascript; charset=utf-8";
+  //       return new Response(new Blob([buffer], { type }));
+  //     }
+  //     return new Response(new Blob(["not found"], { type: "text/plain" }));
+  //   });
+  //   createWindow();
+  //   // mac
+  //   app.on("activate", () => {
+  //     if (BrowserWindow.getAllWindows().length === 0) {
+  //       createWindow();
+  //     }
+  //   });
+  // });
+  // app.on("window-all-closed", () => {
+  //   if (process.platform !== "darwin") {
+  //     app.quit();
+  //   }
+  // });
 }
 
-// import fs from "node:fs";
-// import path from "node:path";
-// import { createServer } from "node:http";
-// import { fileURLToPath } from "node:url";
-// import { spawn } from "node:child_process";
-
-// const html = ([s]: any) => s as string;
-// const page = () => html``;
-
-// const server = createServer((req, res) => {
-//   console.log({ url: req.url });
-//   if (req.url === "/") {
-//     res.writeHead(200).end(page());
-//     return;
-//   }
-// });
-
-// server.listen(9393, "127.0.0.1");
 // http://127.0.0.1:8080/extensions/jpegxl/main.tsx
 // let c = {};
 // 提供一些 trait
