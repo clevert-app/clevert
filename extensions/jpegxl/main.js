39d27c4ae51f06e7f256ca0ed31ec2a853744616
kkocdko <kkocdko@163.com>
2024-05-03 22:21:41 +0800

    remove typescript

---
+++ extensions/jpegxl/main.js
@@ -0,0 +1,68 @@
+import { Extension, AssetKind, ActionKind } from "clevert"; // 这些砍 import 的魔法，在加载扩展的时候做
+import { spawn } from "node:child_process";
+import { Flex, Text, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+export default {
+  id: "jpegxl",
+  name: "jpegxl name",
+  description: "jpegxl description",
+  dependencies: [], // 可以填写其他的 extension 的 id
+  assets: [
+    {
+      platform: "linux-x64",
+      kind: "zip" as AssetKind, // 比如可以做 tar --strip-components 这样的
+      path: "./",
+      url: "https://github.com/clevert-app/clevert/releases/download/make.jpegxl_b2fb216_8900231253/linux-x64.zip",
+    },
+  ],
+  actions: [
+    {
+      id: "cjpegli",
+      name: "cjpegli name",
+      description: "cjpegli description",
+      kind: "converter" as ActionKind, // 还可以是 group-converter，manual converter 之类的？
+      ui: (props: { profile; setProfile; selectedInput }) => {
+        // 这个函数在前端跑，画界面。selectedInput 以后可以用作预览什么的
+        return (
+          <TextField.Root
+            value={props.profile.quality}
+            onChange={(event) => {
+              setProfile((nextProfile) => {
+                return { ...nextProfile, quality: event.target.value };
+              });
+            }}
+            placeholder="Search the docs…"
+          ></TextField.Root>
+        );
+      },
+      execute: ({ onProgress }, profile, input, output) => {
+        // 这个函数在后端跑，要求不 block 主线程，只能 async。如果要 block 请自行开 worker
+        const child = spawn("~/misc/apps/jpegxl", [
+          "cjpegli",
+          input.main[0],
+          output.main[0],
+          "-q",
+          String(profile.quality),
+        ]);
+        child.stderr.on("data", (data: Buffer) => {
+          const chunk = data.toString();
+          onProgress(0.5);
+        });
+        return new Promise((resolve, reject) => {
+          child.on("error", (err) => reject(err));
+          child.on("exit", (code) => (code ? reject({ code }) : resolve(0)));
+        });
+      },
+    },
+  ],
+  profiles: [
+    // 一些预设的 profile
+    {
+      name: "cjpegli default profile name",
+      description: "cjpegli default profile description",
+      action: "cjpegli",
+      quality: 75,
+    },
+  ],
+} as Extension;
---
+++ src/main.js
@@ -0,0 +1,134 @@
+export {}; // let tsserver think this is a es module
+
+type FooBar = {
+  some: string;
+};
+
+if (globalThis.document) {
+  // is in renderer
+  console.log(document);
+  const React = await import("react");
+  const { useState } = await import("react");
+  const { createRoot } = await import("react-dom/client");
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <div>hello react</div>
+    </React.StrictMode>
+  );
+} else {
+  // is in main
+  const { app, protocol, BrowserWindow } = await import("electron");
+  const { fileURLToPath } = await import("node:url");
+  const { readFile } = await import("node:fs/promises");
+  const sucrase = await import("sucrase");
+  sucrase.transform("", {
+    transforms: ["jsx", "typescript"],
+    jsxRuntime: "automatic",
+    disableESTransforms: true,
+    preserveDynamicImport: true,
+  }).code;
+
+  const html = ([s]: any) => s;
+  const page = () => html`
+    <!DOCTYPE html>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width" />
+      <meta name="color-scheme" content="light dark" />
+      <title>clevert</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script type="module" src="resource:///main.js"></script>
+    </body>
+  `;
+
+  const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 1280,
+      height: 720,
+      title: "clevert",
+      webPreferences: {
+        // nodeIntegration: true,
+        contextIsolation: false,
+        webSecurity: false,
+        sandbox: false,
+        // preload: fileURLToPath(import.meta.url),
+      },
+      autoHideMenuBar: true,
+    });
+    win.loadURL("resource:///main.html");
+    win.webContents.openDevTools();
+  };
+
+  app.whenReady().then(() => {
+    protocol.handle("resource", async (req) => {
+      if (req.url === "resource:///main.html") {
+        const type = "text/html; charset=utf-8";
+        return new Response(new Blob([page()], { type }));
+      }
+      if (req.url === "resource:///main.js") {
+        const buffer = await readFile(fileURLToPath(import.meta.url));
+        const type = "text/javascript; charset=utf-8";
+        return new Response(new Blob([buffer], { type }));
+      }
+      return new Response(new Blob(["not found"], { type: "text/plain" }));
+    });
+    createWindow();
+    // mac
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  });
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+}
+
+// import fs from "node:fs";
+// import path from "node:path";
+// import { createServer } from "node:http";
+// import { fileURLToPath } from "node:url";
+// import { spawn } from "node:child_process";
+
+// const html = ([s]: any) => s as string;
+// const page = () => html``;
+
+// const server = createServer((req, res) => {
+//   console.log({ url: req.url });
+//   if (req.url === "/") {
+//     res.writeHead(200).end(page());
+//     return;
+//   }
+// });
+
+// server.listen(9393, "127.0.0.1");
+// http://127.0.0.1:8080/extensions/jpegxl/main.tsx
+// let c = {};
+// 提供一些 trait
+
+// Daemon
+// Transform
+
+// https://registry.npmmirror.com/binary.html?path=electron/v30.0.1/
+// https://registry.npmmirror.com/-/binary/electron/v30.0.1/electron-v30.0.1-linux-x64.zip
+// /home/kkocdko/misc/res/electron-v30.0.1-linux-x64/electron
+// 基于 zustand 封装一个快速表单，然后出 json，无ui时直接用json
+// webPreferences: {
+//   nodeIntegration: true,
+//   contextIsolation: false,
+//   webSecurity: false,
+
+// core -> extension -> action -> config
+// (以后做)  profile = extension + action + config
+
+// https://github.com/clevert-app/notes/issues/1
+
+// 想要知道css怎么弄进来
+// https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/src/components/button.css
+// https://www.radix-ui.com/themes/docs/overview/getting-started
+// node_modules/.bin/sucrase -d src src --transforms typescript,jsx  --jsx-runtime automatic  --disable-es-transforms       
