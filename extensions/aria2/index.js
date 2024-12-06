a6fa475c31f0dce157ac35a11f8ef24adca588f9
kkocdko <kkocdko@gmail.com>
2024-12-07 05:04:25 +0800

    Planing to add aria2 extension

---
+++ extensions/aria2/index.js
@@ -0,0 +1,5 @@
+// https://github.com/mayswind/AriaNg/releases/download/1.3.8/AriaNg-1.3.8-AllInOne.zip
+// https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0-win-64bit-build1.zip
+// https://github.com/P3TERX/Aria2-Pro-Docker
+// https://github.com/P3TERX/aria2.conf
+// https://github.com/P3TERX/Aria2-Pro-Core/
